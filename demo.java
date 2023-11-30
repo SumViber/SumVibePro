da7041d25210de8b57f2a943deb8f79cb4dc0a97
xuxufu <xxf_68621@163.com>
2023-11-30 16:51:47 +0800

    提交代码

---
+++ demo.java
@@ -0,0 +1,9 @@
+/**
+ * @date: 2023/11/30 16:51:07
+ * @author: xuxufu
+ * @description:  
+ */public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
